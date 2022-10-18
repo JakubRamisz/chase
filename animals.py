61c56b32b6571eaf399cf50af13be29a57c2f06b
Jakub Ramisz <jakubramisz01@gmail.com>
2022-10-18 16:33:52 +0200

    Add Sheep class

--- animals.py
+++ animals.py
@@ -1,5 +1,6 @@
 '''Model classes'''
 from abc import ABC, abstractmethod
+import random
 
 
 class Animal(ABC):
@@ -23,3 +24,35 @@ class Animal(ABC):
         '''Move pos by a vector '''
         self.pos[0] += vector[0]
         self.pos[1] += vector[1]
+
+
+class Sheep(Animal):
+    '''
+    Class representing simulated sheep
+
+    Args:
+        init_pos_limit (float): bounds for initial position
+        move_dist (float): distance by which the sheep moves
+
+    Attributes:
+        move_dist (float): used to store move_dist arg
+        pos (list): represents position of the Sheep on cartesian plane
+        alive (boolean): used to determinate wether or not the sheep has been eaten
+    '''
+    def __init__(self, init_pos_limit: float, move_dist: float):
+        super().__init__(move_dist)
+        self.pos = [random.randrange(-init_pos_limit, init_pos_limit),
+                    random.randrange(-init_pos_limit, init_pos_limit)]
+        self.alive = True
+
+    def run(self):
+        '''
+        Randomly chooses a cardinal direction and moves the Sheep by its
+        move_dist in that direction
+        '''
+        direction_selector = [lambda pos: self.move((self.move_dist, 0)),
+                            lambda pos: self.move((-self.move_dist, 0)),
+                            lambda pos: self.move((0, self.move_dist)),
+                            lambda pos: self.move((0, -self.move_dist))]
+
+        random.choice(direction_selector)(self.pos)
