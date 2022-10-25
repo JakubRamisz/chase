83326435c37e0393ee3838d43b0ebce743115094
Jakub Ramisz <jakubramisz01@gmail.com>
2022-10-25 16:52:42 +0200

    Add rounds, sheep and wait optional args

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 '''Main application'''
+import argparse
 from animals import Sheep, Wolf
+from argument_parser import IntRange
 from info_functions import display_info, save_to_json, save_to_csv
 
 
-MAX_ROUND_NUMBER = 20
-SHEEP_FLOCK_SIZE = 15
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-r', '--rounds', help='numer of rounds', metavar='NUM',
+                    type=IntRange(1), default=20)
+parser.add_argument('-s', '--sheep', help='numer of sheep in flock', metavar='NUM',
+                    type=IntRange(1), default=15)
+parser.add_argument('-w', '--wait', help='wait for input after every round', action='store_true')
+
+args = parser.parse_args()
+
+WAIT = args.wait
+MAX_ROUND_NUMBER = args.rounds
+SHEEP_FLOCK_SIZE = args.sheep
 INIT_POS_LIMIT = 10.0
 SHEEP_MOVE_DIST = 0.5
 WOLF_MOVE_DIST = 1.0
@@ -24,8 +37,12 @@ def start_simulation():
             display_info(flock, wolf, round_number)
             save_to_json(flock, wolf, round_number)
             save_to_csv(flock, round_number)
+
+            if WAIT:
+                input()
         else:
             print("All the sheep were eaten")
+            return
 
 
 if __name__ == '__main__':
